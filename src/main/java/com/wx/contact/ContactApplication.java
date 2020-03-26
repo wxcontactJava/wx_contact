421d1923527106415c0d3fdba7fc9026362d3727
liuhy <Lhy18326908228>
2020-03-26 13:45:49 +0800

    测试提交

--- src/main/java/com/wx/contact/ContactApplication.java
+++ src/main/java/com/wx/contact/ContactApplication.java
@@ -8,7 +8,7 @@ public class ContactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactApplication.class, args);
-		System.out.println("测试");
+		System.out.println("测试11");
 	}
 
 }
